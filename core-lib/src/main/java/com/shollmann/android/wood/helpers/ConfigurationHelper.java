9c3d544e919e447afcec330ea1874898ce59f8ed
Santiago Hollmann <santiago.hollmann@gmail.com>
2015-03-04 20:16:47 -0300

    Release v1.00.0

--- core-lib/src/main/java/com/shollmann/android/wood/helpers/ConfigurationHelper.java
+++ core-lib/src/main/java/com/shollmann/android/wood/helpers/ConfigurationHelper.java
@@ -5,5 +5,5 @@ public class ConfigurationHelper {
 
     public static final String MIXPANEL_TEST = "1b8014b454d22e170088e3bb15aee33d";
     public static final String MIXPANEL_PROD = "b91046ee1db6e6024672d0a538684dd8";
-    public static final String MIXPANEL_KEY = MIXPANEL_TEST;
+    public static final String MIXPANEL_KEY = MIXPANEL_PROD;
 }
